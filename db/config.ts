a74931fe578d5603d76e2a5ca14f0e0713ebdfee
Lean Ren <leandro.ren@hotmail.com>
2026-03-08 18:22:31 -0300

    Fix: Import de Vercel y URL de base de datos

--- db/config.ts
+++ db/config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel'; // Cambiado aquí
 import db from '@astrojs/db';
 
 export default defineConfig({
