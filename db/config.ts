c4f5c2b2469731c0ee839ff00c9b5f821e73614d
Lean Ren <leandro.ren@hotmail.com>
2026-03-08 19:18:15 -0300

    Fix: redirects exacto para SSR

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 import db from '@astrojs/db';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [db()],
+  // ESTA LÍNEA ES CLAVE:
+  output: 'server', 
+  adapter: netlify(),
+  integrations: [tailwind(), db()],
 });
